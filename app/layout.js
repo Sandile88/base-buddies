c8b19ae090c3001afeb3cfa09ec3c8da02edf0ff
Sandile88 <sandilep.mremi@gmail.com>
2025-08-12 15:38:23 +0200

    added values for .env vars

--- app/layout.js
+++ app/layout.js
@@ -4,22 +4,19 @@ import { MiniKitContextProvider } from '../providers/MiniKitProvider';
 export const metadata = {
   title: 'Base Buddies',
   description: 'A fun onchain app built on Base!',
-  // other: {
-  //   "fc:frame": JSON.stringify({
-  //     version: "next",
-  //     imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
-  //     button: {
-  //       title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'Base Buddies'}`,
-  //       action: {
-  //         type: "launch_frame",
-  //         name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'Base Buddies',
-  //         url: process.env.NEXT_PUBLIC_URL,
-  //         splashImageUrl: process.env.NEXT_PUBLIC_SPLASH_IMAGE,
-  //         splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
-  //       },
-  //     },
-  //   }),
-  // },
+  other: {
+    "fc:frame": JSON.stringify({
+      version: "next",
+      button: {
+        title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'Base Buddies'}`,
+        action: {
+          type: "launch_frame",
+          name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'Base Buddies',
+          url: process.env.NEXT_PUBLIC_URL,
+        },
+      },
+    }),
+  },
 };
 
 export const viewport = {
